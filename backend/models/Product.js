fd311ffaff024d3ff54c34c57975ce617623bf70
angelporlan <angeelporlan4@gmail.com>
2025-01-11 19:34:55 +0100

    add Product model definition with Sequelize

---
+++ backend/models/Product.js
@@ -0,0 +1,9 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Product = sequelize.define('Product', {
+    name: { type: DataTypes.STRING, allowNull: false },
+    image: { type: DataTypes.STRING },
+});
+
+module.exports = Product;
