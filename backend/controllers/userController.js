03f6c49cc8f459b86f6627e6341db9c0fa3eec81
angelporlan <angeelporlan4@gmail.com>
2025-01-12 12:06:24 +0100

    add getUserProfile function to retrieve user profile information

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,4 +29,13 @@ const loginUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser, loginUser };
+const getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.user.id, { attributes: { exclude: ['password'] } });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = { registerUser, loginUser, getUserProfile };
