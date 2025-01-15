25a781fc98cfc748e85e758de0e0d4be46c6a108
angelporlan <angeelporlan4@gmail.com>
2025-01-15 13:01:29 +0100

    add productDetail routes with authentication and validation

---
+++ backend/routes/productDetailRoutes.js
@@ -0,0 +1,18 @@
+const { express } = require('express');
+
+const {
+    createProductDetail,
+    updateProductDetail,
+    deleteProductDetail,
+} = require('../controllers/ProductDetailController');
+
+const authenticateToken = require('../middlewares/authenticateToken');
+const validateProductDetail = require('../middlewares/validateProductDetail');
+
+const router = express.Router();
+
+router.post('/', authenticateToken, validateProductDetail, createProductDetail);
+router.put('/:id', authenticateToken, validateProductDetail, updateProductDetail);
+router.delete('/:id', authenticateToken, deleteProductDetail);
+
+module.exports = router;
