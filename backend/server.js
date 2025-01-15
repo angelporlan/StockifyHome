f0a7c3916268724818e6dfe2ede8f09d4dceb0f1
angelporlan <angeelporlan4@gmail.com>
2025-01-15 13:01:37 +0100

    add productDetail routes to server for API integration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const { sequelize } = require('./models');
+
 const userRoutes = require('./routes/userRoutes');
 const houseRoutes = require('./routes/houseRoutes');
 const productRoutes = require('./routes/productRoutes');
+const productDetailRoutes = require('./routes/productDetailRoutes');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/houses', houseRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/productDetails', productDetailRoutes);
 
 const PORT = process.env.PORT || 3000;
 
