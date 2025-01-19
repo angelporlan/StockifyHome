ad2805361a877c4643e5ae2d8aaf39e0fe96f23b
angelporlan <angeelporlan4@gmail.com>
2025-01-19 17:33:48 +0100

    feat: implement authentication component with login and signup functionality

--- frontend/src/app/views/auth/auth.component.ts
+++ frontend/src/app/views/auth/auth.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  isLoading: boolean = false;
+  loginActive: boolean = true;
+  email: any;
+  password: any;
+  username: any;
+  submitText: string = 'Login';
+  
+  constructor(private router: Router,
+    private authService: AuthService,
+    // private snackBar: MatSnackBar
+  ) { }
+
+  showMessage(message: string): void {
+    // this.snackBar.open(message, 'Close', {
+    //   duration: 3000,
+    //   verticalPosition: 'top',
+    //   horizontalPosition: 'center'
+    // });
+  }
+
+  toggleAuth(value: boolean): void {
+    this.loginActive = value;
+    this.submitText = value ? 'Login' : 'Register';
+  }
+
+  login(): void {
+
+  }
+
+  register(): void {
+
+  }
+
+  navigateTo(path: string): void {
+    this.router.navigate([path]);
+  }
 
 }
