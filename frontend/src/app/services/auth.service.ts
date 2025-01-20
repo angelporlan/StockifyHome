26b3c997fd6b62097c7bb5ca3df3a2ccec595d66
angelporlan <angeelporlan4@gmail.com>
2025-01-20 11:19:18 +0100

    feat: implement login and registration methods in AuthService

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl: string = 'http://127.0.0.1:3000/api/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+  register(email: string, password: string, username: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, { email, password, username });
+  }
 }
