5abfe3d21cf581ea67c2e356d3945cb144d4bc19
angelporlan <angeelporlan4@gmail.com>
2025-01-20 11:15:47 +0100

    feat: enable CORS support in the backend server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const { sequelize } = require('./models');
 
@@ -8,6 +9,7 @@ const productRoutes = require('./routes/productRoutes');
 const productDetailRoutes = require('./routes/productDetailRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
