1f6bf37a1811e7b53b270d291b71c2b0f1279af1
angelporlan <angeelporlan4@gmail.com>
2025-01-20 11:19:25 +0100

    feat: enhance login and registration methods with loading state and error handling

--- frontend/src/app/views/auth/auth.component.ts
+++ frontend/src/app/views/auth/auth.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
@@ -38,11 +38,37 @@ export class AuthComponent {
   }
 
   login(): void {
-
+    this.isLoading = true;
+    console.log(this.email, this.password);
+    this.authService.login(this.email, this.password).subscribe(
+      (res) => {
+        this.isLoading = false;
+        console.log(res)
+        console.log('Login successful');
+        console.log('home');
+      },
+      (err) => {
+        this.isLoading = false;
+        console.log('Login failed', err);
+      }
+    );
   }
 
   register(): void {
-
+    this.isLoading = true;
+    console.log(this.email, this.password, this.username);
+    this.authService.register(this.email, this.password, this.username).subscribe(
+      (res) => {
+        this.isLoading = false;
+        console.log(res)
+        console.log('Registration successful');
+        console.log('home');
+      },
+      (err) => {
+        this.isLoading = false;
+        console.log('Registration failed');
+      }
+    );
   }
 
   navigateTo(path: string): void {
