57886e63ef8fd2d8ad9b37024964c8539ee7d781
angelporlan <angeelporlan4@gmail.com>
2025-01-21 22:05:16 +0100

    feat: update SidebarComponent to navigate to 'main' instead of 'dashboard'

--- frontend/src/app/components/dashboard/sidebar/sidebar.component.ts
+++ frontend/src/app/components/dashboard/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -13,21 +13,16 @@ export class SidebarComponent {
   currtentRoute: string = '';
   isSidebarHidden: boolean = false;
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router) { }
 
   navigateTo(path: string): void {
-    const currentRoute = this.router.url;
-    const segments = currentRoute.split('/');
-    const dashboardIndex = segments.indexOf('dashboard');
-    const id = segments[dashboardIndex + 1];
-    const newRoute = `dashboard/${id}/${path}`;
+    const newRoute = `dashboard/${path}`;
     this.router.navigate([newRoute]);
   }
 
   isActive(path: string): boolean {
     const currentRoute = this.router.url;
     const segments = currentRoute.split('/');
-    console.log(segments.includes(path))
     return segments.includes(path);
   }
 
