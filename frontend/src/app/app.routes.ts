5f4c456246b4d72d25aaa7cdef3a308334b4ea70
angelporlan <angeelporlan4@gmail.com>
2025-01-21 18:18:45 +0100

    feat: add dashboard route to application routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 //views
 import { AuthComponent } from './views/auth/auth.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: AuthComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ]
