aadc05ad2902965b336597066fc1f79ca22ae94e
angelporlan <angeelporlan4@gmail.com>
2025-01-21 22:05:53 +0100

    feat: update routing to include children for DashboardComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 
-//views
+// views
 import { AuthComponent } from './views/auth/auth.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+
+// dashboard children
+import { MainContentComponent } from './views/dashboard/main-content/main-content.component';
+import { HousesComponent } from './views/dashboard/houses/houses.component';
+import { ProductsComponent } from './views/dashboard/products/products.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: AuthComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, children: [
+    { path: 'main', component: MainContentComponent },
+    { path: 'houses', component: HousesComponent },
+    { path: 'products', component: ProductsComponent },
+  ]
+  },
 ]
