c66ad366d6109f5806ad946a6dd7d1dce85b16f3
angelporlan <angeelporlan4@gmail.com>
2025-01-22 13:28:56 +0100

    feat: integrate InputSearchComponent into HousesComponent for house search functionality

--- frontend/src/app/views/dashboard/houses/houses.component.ts
+++ frontend/src/app/views/dashboard/houses/houses.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { InputSearchComponent } from '../../../components/dashboard/input-search/input-search.component';
 
 @Component({
   selector: 'app-houses',
-  imports: [],
+  imports: [InputSearchComponent],
   templateUrl: './houses.component.html',
   styleUrl: './houses.component.css'
 })
 export class HousesComponent {
 
+
+  onSearchChange(event: any) {
+    console.log(event);
+  }
 }
