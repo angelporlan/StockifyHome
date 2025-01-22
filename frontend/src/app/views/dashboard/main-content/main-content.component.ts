273660115a0afc8773be233ead6bcccf18d40a0d
angelporlan <angeelporlan4@gmail.com>
2025-01-22 11:32:32 +0100

    feat: integrate InfoBoxesComponent into MainContentComponent

--- frontend/src/app/views/dashboard/main-content/main-content.component.ts
+++ frontend/src/app/views/dashboard/main-content/main-content.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { InfoBoxesComponent } from '../../../components/dashboard/main-content/info-boxes/info-boxes.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [InfoBoxesComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
