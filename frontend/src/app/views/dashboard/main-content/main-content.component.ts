3db7dfd5fde02f148ba502183e69caab0488766b
angelporlan <angeelporlan4@gmail.com>
2025-01-22 12:47:22 +0100

    feat: add QuickActionsComponent to MainContentComponent

--- frontend/src/app/views/dashboard/main-content/main-content.component.ts
+++ frontend/src/app/views/dashboard/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InfoBoxesComponent } from '../../../components/dashboard/main-content/info-boxes/info-boxes.component';
 import { TitleComponent } from '../../../components/dashboard/title/title.component';
+import { QuickActionsComponent } from '../../../components/dashboard/main-content/quick-actions/quick-actions.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [InfoBoxesComponent, TitleComponent],
+  imports: [InfoBoxesComponent, TitleComponent, QuickActionsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
