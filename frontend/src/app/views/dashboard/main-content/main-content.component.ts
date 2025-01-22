bc6666f4b4822dc76289c5431fe4673c2a11e949
angelporlan <angeelporlan4@gmail.com>
2025-01-22 11:42:20 +0100

    feat: add TitleComponent to MainContentComponent

--- frontend/src/app/views/dashboard/main-content/main-content.component.ts
+++ frontend/src/app/views/dashboard/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { InfoBoxesComponent } from '../../../components/dashboard/main-content/info-boxes/info-boxes.component';
+import { TitleComponent } from '../../../components/dashboard/title/title.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [InfoBoxesComponent],
+  imports: [InfoBoxesComponent, TitleComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
