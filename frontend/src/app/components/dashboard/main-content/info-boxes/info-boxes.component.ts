08718a065010c06d719f88033b023959572a89d0
angelporlan <angeelporlan4@gmail.com>
2025-01-22 11:32:28 +0100

    feat: add InfoBoxesComponent with template, styles, and unit tests

---
+++ frontend/src/app/components/dashboard/main-content/info-boxes/info-boxes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InfoBoxComponent } from './info-box/info-box.component';
+
+@Component({
+  selector: 'app-info-boxes',
+  imports: [InfoBoxComponent],
+  templateUrl: './info-boxes.component.html',
+  styleUrl: './info-boxes.component.css'
+})
+export class InfoBoxesComponent {
+
+}
