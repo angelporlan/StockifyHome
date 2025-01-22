90bdd23bc525d7785ed437e94089501e5d96a66d
angelporlan <angeelporlan4@gmail.com>
2025-01-22 13:28:50 +0100

    feat: integrate InputSearchComponent into ProductsComponent for product search functionality

--- frontend/src/app/views/dashboard/products/products.component.ts
+++ frontend/src/app/views/dashboard/products/products.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { InputSearchComponent } from '../../../components/dashboard/input-search/input-search.component';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [InputSearchComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-
+  onSearchChange(event: any) {
+    console.log(event);
+  }
 }
