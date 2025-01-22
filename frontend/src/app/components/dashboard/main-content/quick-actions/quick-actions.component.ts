11b9711a7896f98af677815185d15f873e4ff53f
angelporlan <angeelporlan4@gmail.com>
2025-01-22 12:47:16 +0100

    feat: add QuickActionsComponent with template, styles, and unit tests

---
+++ frontend/src/app/components/dashboard/main-content/quick-actions/quick-actions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { QuickActionComponent } from './quick-action/quick-action.component';
+
+@Component({
+  selector: 'app-quick-actions',
+  imports: [QuickActionComponent],
+  templateUrl: './quick-actions.component.html',
+  styleUrl: './quick-actions.component.css'
+})
+export class QuickActionsComponent {
+
+}
