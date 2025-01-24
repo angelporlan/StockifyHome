0c606d952225bf799dad7141aca16c668f60f26d
angelporlan <angeelporlan4@gmail.com>
2025-01-24 16:41:49 +0100

    feat: integrate HouseBoxes component into Houses view for enhanced house display

--- frontend/src/app/views/dashboard/houses/houses.component.ts
+++ frontend/src/app/views/dashboard/houses/houses.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { InputSearchComponent } from '../../../components/dashboard/input-search/input-search.component';
+import { HouseBoxesComponent } from '../../../components/dashboard/houses/house-boxes/house-boxes.component';
 
 @Component({
   selector: 'app-houses',
-  imports: [InputSearchComponent],
+  imports: [InputSearchComponent, HouseBoxesComponent],
   templateUrl: './houses.component.html',
   styleUrl: './houses.component.css'
 })
 export class HousesComponent {
-
 
   onSearchChange(event: any) {
     console.log(event);
