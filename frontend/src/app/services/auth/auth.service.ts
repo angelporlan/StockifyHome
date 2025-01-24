4acdcb1248f489e9f129f60479676c7b8ac2441b
angelporlan <angeelporlan4@gmail.com>
2025-01-24 11:14:07 +0100

    refactor: move AuthService and its tests to a dedicated auth directory

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AuthStore } from '../store/auth.store';
+import { AuthStore } from '../../store/auth.store';
 
 @Injectable({
   providedIn: 'root'
