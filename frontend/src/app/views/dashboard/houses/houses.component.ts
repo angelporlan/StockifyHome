8a8943241fa208f98b284b17a3df715c7a1ee234
angelporlan <angeelporlan4@gmail.com>
2025-01-25 12:41:39 +0100

    feat: add search button layout and integrate ActionButton in houses component

--- frontend/src/app/views/dashboard/houses/houses.component.ts
+++ frontend/src/app/views/dashboard/houses/houses.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputSearchComponent } from '../../../components/dashboard/input-search/input-search.component';
 import { HouseBoxesComponent } from '../../../components/dashboard/houses/house-boxes/house-boxes.component';
+import { ActionButtonComponent } from '../../../components/dashboard/action-button/action-button.component';
 
 @Component({
   selector: 'app-houses',
-  imports: [InputSearchComponent, HouseBoxesComponent],
+  imports: [InputSearchComponent, HouseBoxesComponent, ActionButtonComponent],
   templateUrl: './houses.component.html',
   styleUrl: './houses.component.css'
 })
