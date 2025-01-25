e4c1b0e18c583ca7fee2e2ae5dbbd46619aed1b3
angelporlan <angeelporlan4@gmail.com>
2025-01-25 12:41:44 +0100

    feat: implement search button layout with integrated ActionButton in products component

--- frontend/src/app/views/dashboard/products/products.component.ts
+++ frontend/src/app/views/dashboard/products/products.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { InputSearchComponent } from '../../../components/dashboard/input-search/input-search.component';
+import { ActionButtonComponent } from "../../../components/dashboard/action-button/action-button.component";
 
 @Component({
   selector: 'app-products',
-  imports: [InputSearchComponent],
+  imports: [InputSearchComponent, ActionButtonComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
