89207472a050e853c575e0eade1c361256b7a5f0
angelporlan <angeelporlan4@gmail.com>
2025-01-25 12:41:22 +0100

    feat: add ActionButton component with customizable text and action types

---
+++ frontend/src/app/components/dashboard/action-button/action-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-action-button',
+  imports: [],
+  templateUrl: './action-button.component.html',
+  styleUrl: './action-button.component.css'
+})
+export class ActionButtonComponent {
+  @Input() text: string = 'Button';
+  @Input() actionType: 'addHouse' | 'editHouse' | 'deleteHouse' | 
+  'addProduct' | 'editProduct' | 'deleteProduct' = 'addHouse';
+}
