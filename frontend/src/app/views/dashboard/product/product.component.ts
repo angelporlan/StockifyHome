9fde28c1735f60925086de3c02249e7b157195ff
angelporlan <angeelporlan4@gmail.com>
2025-01-26 19:24:59 +0100

    feat: integrate DetailsComponent and ProductDetailsComponent in ProductComponent

--- frontend/src/app/views/dashboard/product/product.component.ts
+++ frontend/src/app/views/dashboard/product/product.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DetailsComponent } from '../../../components/dashboard/product/details/details.component';
+import { ProductStore } from '../../../store/product.store';
+import { ProductDetailsComponent } from '../../../components/dashboard/product/product-details/product-details.component';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [DetailsComponent, ProductDetailsComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  productStore = inject(ProductStore);
+  
+  getIdFromUrl(): number {
+    const url = window.location.href;
+    const segments = url.split('/');
+    return parseInt(segments[segments.length - 1]);
+  }
 
+  getProductById(id: number) {
+    const products = this.productStore.selectedProducts();
+    return products.find(p => p.id === id);;
+  }
 }
