b467315a1d4d4168d709a8db420f8fbf15e400af
angelporlan <angeelporlan4@gmail.com>
2025-01-26 19:24:21 +0100

    feat: add DetailsTableComponent to display product details with formatted date and quantity

---
+++ frontend/src/app/components/dashboard/product/product-details/details-table/details-table.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { ProductDetail } from '../../../../../interfaces/product-detail';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-details-table',
+  imports: [CommonModule],
+  templateUrl: './details-table.component.html',
+  styleUrl: './details-table.component.css'
+})
+export class DetailsTableComponent {
+  @Input() details: ProductDetail[] = [{
+    id: 0,
+    quantity: 0,
+    expiration_date: ''
+  }];
+  
+
+  getFormattedDate(date: string): string {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric'
+    });
+  }
+  
+}
