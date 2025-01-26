f8ff4099b6c185822dc72cabf72ef6c59e85cbf7
angelporlan <angeelporlan4@gmail.com>
2025-01-26 16:23:24 +0100

    feat: add ProductBoxesComponent to products view for enhanced product display

--- frontend/src/app/views/dashboard/products/products.component.ts
+++ frontend/src/app/views/dashboard/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputSearchComponent } from '../../../components/dashboard/input-search/input-search.component';
 import { ActionButtonComponent } from "../../../components/dashboard/action-button/action-button.component";
+import { ProductBoxesComponent } from '../../../components/dashboard/products/product-boxes/product-boxes.component';
 
 @Component({
   selector: 'app-products',
-  imports: [InputSearchComponent, ActionButtonComponent],
+  imports: [InputSearchComponent, ActionButtonComponent, ProductBoxesComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
