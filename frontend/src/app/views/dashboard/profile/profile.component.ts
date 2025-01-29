2ac787f74642af9ff3aa3c5cc076ad146cf3c865
angelporlan <angeelporlan4@gmail.com>
2025-01-30 00:41:13 +0100

    feat: integrate InputModal for password change and update profile functionality

--- frontend/src/app/views/dashboard/profile/profile.component.ts
+++ frontend/src/app/views/dashboard/profile/profile.component.ts
@@ -2,6 +2,10 @@ import { Component, inject } from '@angular/core';
 import { AuthStore } from '../../../store/auth.store';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
+import { InputModalComponent } from '../../../components/general/modals/input-modal/input-modal.component';
+import { AuthService } from '../../../services/auth/auth.service';
+import { MatSnackBarService } from '../../../services/matSnackBar/mat-snack-bar.service';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -12,7 +16,7 @@ import { MatDialog } from '@angular/material/dialog';
 export class ProfileComponent {
   authStore = inject(AuthStore);
 
-  constructor(private router: Router, public dialog: MatDialog) {}
+  constructor(private router: Router, public dialog: MatDialog, private authService: AuthService, private matSnackBarService: MatSnackBarService) {}
 
   closeSession(): void {
     this.authStore.deleteToken();
@@ -20,6 +24,27 @@ export class ProfileComponent {
   }
 
   openDialog(): void {
+    this.dialog.open(InputModalComponent, {
+      width: '400px',
+      data: {
+        title: 'Change password',
+        labelInput: 'Password',
+        placeholderInput: 'Enter your new password',
+        action: (newPassword: string) => this.updateProfile(newPassword)
+      }
+    });
+  }
 
+  updateProfile(newPassword: string) {
+    return this.authService.updateProfile(undefined, undefined, newPassword).pipe(
+      tap(() => {
+        this.authService.updateProfile(newPassword);
+        this.matSnackBarService.showSuccess('Profile updated successfully');
+      }),
+      catchError((error) => {
+        this.matSnackBarService.showError(error.error.error);
+        return throwError(() => new Error('Error updating profile'));
+      })
+    );
   }
 }
