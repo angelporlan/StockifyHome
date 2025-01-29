0372ad5ecd4d13be5668838c4d4932e7d7bf5671
angelporlan <angeelporlan4@gmail.com>
2025-01-29 20:13:01 +0100

    feat: enhance confirm modal component to handle dynamic data and close dialog after action

--- frontend/src/app/components/general/modals/confirm-modal/confirm-modal.component.ts
+++ frontend/src/app/components/general/modals/confirm-modal/confirm-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Inject } from '@angular/core';
 
 @Component({
   selector: 'app-confirm-modal',
@@ -12,7 +14,17 @@ export class ConfirmModalComponent {
   @Input() message: string = '';  
   @Input() action: Function = () => {};
 
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.title = data.title;
+    this.message = data.message;
+    this.action = data.action;
+  }
+
   actionModal() {
     this.action();
+    this.dialogRef.close();
   }
 }
