ef32335982172b7ebd422b5b819354f6cbb6d509
angelporlan <angeelporlan4@gmail.com>
2025-01-29 20:13:10 +0100

    feat: add confirmation dialog for product deletion in details component

--- frontend/src/app/components/dashboard/product/details/details.component.ts
+++ frontend/src/app/components/dashboard/product/details/details.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TitleComponent } from '../../title/title.component';
 import { ItemCardComponent } from '../../item-card/item-card.component';
 import { ActionButtonComponent } from '../../action-button/action-button.component';
-import { ProductStore } from '../../../../store/product.store';
 import { Product } from '../../../../interfaces/product';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmModalComponent } from '../../../general/modals/confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-details',
@@ -28,7 +29,7 @@ export class DetailsComponent {
     ProductDetails: []
   }
 
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.validProduct();
@@ -39,6 +40,17 @@ export class DetailsComponent {
       this.realProduct = this.product;
       console.log(this.product);
     }
+  }
+
+  openDialog(): void {
+    this.dialog.open(ConfirmModalComponent, {
+      width: '400px',
+      data: {
+        title: 'Eliminar producto',
+        message: '¿Estás seguro de que quieres eliminar este producto?',
+        action: () => { console.log('Producto eliminado'); } 
+      }
+    });
   }
 
 }
