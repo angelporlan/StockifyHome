c3144e848a72ebafd5b57f627a90e3d8ad21d6fe
angelporlan <angeelporlan4@gmail.com>
2025-01-29 20:47:52 +0100

    feat: enhance confirm modal to show loading state and handle async actions

--- frontend/src/app/components/general/modals/confirm-modal/confirm-modal.component.ts
+++ frontend/src/app/components/general/modals/confirm-modal/confirm-modal.component.ts
@@ -2,10 +2,13 @@ import { Component, Input } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-confirm-modal',
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, MatButton, CommonModule],
   templateUrl: './confirm-modal.component.html',
   styleUrl: './confirm-modal.component.css'
 })
@@ -13,6 +16,7 @@ export class ConfirmModalComponent {
   @Input() title: string = '';
   @Input() message: string = '';  
   @Input() action: Function = () => {};
+  isLoading: boolean = false;
 
   constructor(
     public dialogRef: MatDialogRef<ConfirmModalComponent>,
@@ -24,7 +28,21 @@ export class ConfirmModalComponent {
   }
 
   actionModal() {
-    this.action();
-    this.dialogRef.close();
+    this.isLoading = true; 
+  
+    const result = this.action();
+  
+    if (result instanceof Promise) {
+      result
+        .then(() => this.dialogRef.close()) 
+        .catch(() => (this.isLoading = false));
+    } else if (result instanceof Observable) {
+      result.subscribe({
+        next: () => this.dialogRef.close(),
+        error: () => (this.isLoading = false),
+      });
+    } else {
+      this.dialogRef.close();
+    }
   }
 }
