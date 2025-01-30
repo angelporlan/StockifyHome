c7b05ef8c0887423ac2532eda1fa1efc4aa8b165
angelporlan <angeelporlan4@gmail.com>
2025-01-30 13:08:55 +0100

    feat: add product filtering functionality to product boxes component

--- frontend/src/app/components/dashboard/products/product-boxes/product-boxes.component.ts
+++ frontend/src/app/components/dashboard/products/product-boxes/product-boxes.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ProductStore } from '../../../../store/product.store';
 import { ItemCardComponent } from '../../item-card/item-card.component';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from '../../title/title.component';
+import { Product } from '../../../../interfaces/product';
 
 @Component({
   selector: 'app-product-boxes',
@@ -13,4 +14,13 @@ import { TitleComponent } from '../../title/title.component';
 export class ProductBoxesComponent {
   productStore = inject(ProductStore);
   text: string = 'Not products found in this house, try adding one!';
+  @Input() searchText: string = '';
+
+  filterProducts(products: Product[]) {
+    if (this.searchText === '') {
+      return products;
+    }
+    const searchTextLower = this.searchText.toLowerCase();
+    return products.filter((product: any) => product.name.toLowerCase().includes(searchTextLower));
+  }
 }
