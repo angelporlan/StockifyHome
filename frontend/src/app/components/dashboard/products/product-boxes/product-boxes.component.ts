1eb7b855d6ed8bcf104acc8396af5178d4776d17
angelporlan <angeelporlan4@gmail.com>
2025-01-30 12:49:31 +0100

    feat: add title component to display message when no products are selected

--- frontend/src/app/components/dashboard/products/product-boxes/product-boxes.component.ts
+++ frontend/src/app/components/dashboard/products/product-boxes/product-boxes.component.ts
@@ -2,13 +2,15 @@ import { Component, inject } from '@angular/core';
 import { ProductStore } from '../../../../store/product.store';
 import { ItemCardComponent } from '../../item-card/item-card.component';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '../../title/title.component';
 
 @Component({
   selector: 'app-product-boxes',
-  imports: [ItemCardComponent, CommonModule],
+  imports: [ItemCardComponent, CommonModule, TitleComponent],
   templateUrl: './product-boxes.component.html',
   styleUrl: './product-boxes.component.css'
 })
 export class ProductBoxesComponent {
   productStore = inject(ProductStore);
+  text: string = 'Not products found in this house, try adding one!';
 }
