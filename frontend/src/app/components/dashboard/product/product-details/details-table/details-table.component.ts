571a02015625577a419b4bc5ee0e902bcc9d7686
angelporlan <angeelporlan4@gmail.com>
2025-01-31 13:37:15 +0100

    feat: enhance details table to manage product quantity updates with loading state

--- frontend/src/app/components/dashboard/product/product-details/details-table/details-table.component.ts
+++ frontend/src/app/components/dashboard/product/product-details/details-table/details-table.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ProductDetail } from '../../../../../interfaces/product-detail';
 import { CommonModule } from '@angular/common';
 import { CounterComponent } from '../counter/counter.component';
+import { ProductService } from '../../../../../services/product/product.service';
+import { ProductStore } from '../../../../../store/product.store';
 
 @Component({
   selector: 'app-details-table',
@@ -10,19 +12,40 @@ import { CounterComponent } from '../counter/counter.component';
   styleUrl: './details-table.component.css'
 })
 export class DetailsTableComponent {
+  @Input() productId: number | undefined = 0;
   @Input() details: ProductDetail[] = [{
     id: 0,
     quantity: 0,
     expiration_date: ''
   }];
-  
 
+  isLoading: boolean[] = [];
+  productStore = inject(ProductStore);
+
+  constructor(private productService: ProductService) {}
+  
   getFormattedDate(date: string): string {
     const dateObj = new Date(date);
     return dateObj.toLocaleDateString('en-US', {
       month: 'short',
       day: 'numeric',
       year: 'numeric'
+    });
+  }
+
+  updateDetail(event: any): void {
+    this.isLoading[event.id] = true;
+    this.productService.updateProductDetail(event).subscribe({
+      next: () => {
+        if (this.productId !== undefined) {
+          console.log(this.productId);
+          console.log(event.id);
+          console.log(event.quantity);
+          this.productStore.updateProductDetail(this.productId, event.id, event.quantity);
+        }
+        this.isLoading[event.id] = false;
+      }
+
     });
   }
   
