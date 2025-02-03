d10875d7c05c6c84e8fdbdb3afb2358e136d0533
angelporlan <angeelporlan4@gmail.com>
2025-02-03 12:59:59 +0100

    feat: update product detail validation to handle POST and PUT methods separately

--- backend/middlewares/validateProductDetail.js
+++ backend/middlewares/validateProductDetail.js
@@ -1,4 +1,5 @@
 const { ProductDetail } = require('../models');
+
 const validateProductDetail = async (req, res, next) => {
     try {
         let productDetails = req.body;
@@ -10,21 +11,27 @@ const validateProductDetail = async (req, res, next) => {
         for (const detail of productDetails) {
             const { quantity, expiration_date, product_id } = detail;
 
-            if (quantity === undefined || !expiration_date || !product_id) {
-                return res.status(400).json({ error: 'Quantity, expiration_date, and product_id are required' });
-            }
+            if (req.method === 'PUT') {
+                if (quantity < 0) {
+                    return res.status(400).json({ error: 'Quantity must be greater or equal to 0' });
+                }
+            } else if (req.method === 'POST') {
+                if (quantity === undefined || !expiration_date || !product_id) {
+                    return res.status(400).json({ error: 'Quantity, expiration_date, and product_id are required' });
+                }
 
-            if (isNaN(Date.parse(expiration_date))) {
-                return res.status(400).json({ error: 'Expiration date must be a valid date' });
-            }
+                if (isNaN(Date.parse(expiration_date))) {
+                    return res.status(400).json({ error: 'Expiration date must be a valid date' });
+                }
 
-            if (quantity < 0) {
-                return res.status(400).json({ error: 'Quantity must be greater or equal to 0' });
-            }
+                if (quantity < 0) {
+                    return res.status(400).json({ error: 'Quantity must be greater or equal to 0' });
+                }
 
-            const existingProductDetail = await ProductDetail.findOne({ where: { expiration_date, product_id } });
-            if (existingProductDetail) {
-                return res.status(400).json({ error: 'ProductDetail with the same expiration date already exists' });
+                const existingProductDetail = await ProductDetail.findOne({ where: { expiration_date, product_id } });
+                if (existingProductDetail) {
+                    return res.status(400).json({ error: 'ProductDetail with the same expiration date already exists' });
+                }
             }
         }
 
