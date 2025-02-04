8c4e432aa52e8a3939ce8ea9a14c883c0dc5d6d9
angelporlan <angeelporlan4@gmail.com>
2025-02-04 12:50:53 +0100

    feat: add imageUrl input binding to DefaultInputFileComponent

--- frontend/src/app/components/general/inputs/default-input-file/default-input-file.component.ts
+++ frontend/src/app/components/general/inputs/default-input-file/default-input-file.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-input-file',
@@ -8,7 +8,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrl: './default-input-file.component.css'
 })
 export class DefaultInputFileComponent {
-  imageUrl: string | ArrayBuffer | null = null;
+  @Input() imageUrl: string | ArrayBuffer | null = null;
   @Output() imageSelected = new EventEmitter<string>();
 
   onFileSelected(event: Event): void {
