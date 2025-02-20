5cc96707bcb7e918fbae40e8bc6a4216d26897e1
angelporlan <angeelporlan4@gmail.com>
2025-02-20 12:15:34 +0100

    refactor: remove commented-out local database configuration from database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -14,7 +14,6 @@ const sequelize = new Sequelize(process.env.DB_URL, {
 module.exports = sequelize;
 
 // LOCAL DATABASE
-
 // const { Sequelize } = require('sequelize');
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 //     host: process.env.DB_HOST,
