3a45fd4c385ad84a12e1b6275116dcf81caccf25
angelporlan <angeelporlan4@gmail.com>
2025-02-20 11:59:49 +0100

    feat: update database configuration to use PostgreSQL with SSL support

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,24 @@
+// DATABASE CONNECTION IN SUPABASE
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',  
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
 
 module.exports = sequelize;
+
+// LOCAL DATABASE
+
+// const { Sequelize } = require('sequelize');
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+// });
+
+// module.exports = sequelize;
