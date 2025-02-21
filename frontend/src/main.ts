6a1d1f9062103b42ea1d5e45e9b9b129fb3eac24
angelporlan <angeelporlan4@gmail.com>
2025-02-21 16:39:53 +0100

    feat: integrate ngx-translate for multilingual support and set default language to Spanish

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,39 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
+
+const translateProviders = TranslateModule.forRoot({
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+}).providers;
+
+const updatedAppConfig = {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    provideAnimations(),
+    ...(translateProviders || []), 
+  ],
+};
+
+bootstrapApplication(AppComponent, updatedAppConfig)
+  .then(appRef => {
+    const translate = appRef.injector.get(TranslateService);
+    translate.setDefaultLang('es');
+    translate.use('es');
+  })
+  .catch(err => console.error(err));
+
+
+
