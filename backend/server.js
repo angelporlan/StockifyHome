ab85f3c785f2a7674b319fe396c8dd7c8996d7e8
angelporlan <angeelporlan4@gmail.com>
2025-03-04 10:43:09 +0100

    feat: add Swagger documentation for API with swagger-jsdoc and swagger-ui-express

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const { sequelize } = require('./models');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const userRoutes = require('./routes/userRoutes');
 const houseRoutes = require('./routes/houseRoutes');
@@ -9,9 +11,27 @@ const productRoutes = require('./routes/productRoutes');
 const productDetailRoutes = require('./routes/productDetailRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "StockifyHome API",
+            version: "1.0.0",
+            description: "Documentation for StockifyHome API",
+        },
+        servers: [
+            { url: "http://localhost:3000" }, // localhost
+            { url: "https://stockifyhome.onrender.com" } // render
+          ],
+    },
+    apis: ["./routes/*.js"],
+};
+const specs = swaggerJsdoc(options);
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use('/api/users', userRoutes);
 app.use('/api/houses', houseRoutes);
